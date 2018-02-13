cd02571752365110c9448b0f48868a8e2056f40c
An Nuyen <annuyen@gmail.com>
2018-02-13 19:46:11 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,10 +37,9 @@ class Student
   	end
   end
 
-  def self.create
+  def self.create(name, grade)
     student = Student.new(name, grade)
     student.save
-    student
   end
 
   def self.new_from_db(row)
