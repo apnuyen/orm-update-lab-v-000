a1f688a694681c72e8bdba77fd3c8ecce5d0204e
An Nuyen <annuyen@gmail.com>
2018-02-13 19:39:07 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -5,6 +5,9 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   def initialize(name, grade, id=nil)
+    @id = id
+    @name = name
+    @grade = grade
   end
 
   def self.create_table
